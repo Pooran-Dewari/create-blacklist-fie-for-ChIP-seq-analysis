496f3953f795c63f97a31799cea57255bc98eb34
Pooran-Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2022-12-03 11:54:33 +0000

    Create mv_to_temps.sh

---
+++ mv_to_temps.sh
@@ -0,0 +1,6 @@
+# move kmer and bowtie gz files to TEMPs
+
+mv data/TestGenomeMappability/kmers/k100/*kmer* data/TestGenomeMappability/kmers/k100/TEMPs
+mv data/TestGenomeMappability/kmers/k100/*bowtie* data/TestGenomeMappability/kmers/k100/TEMPs
+mv data/TestGenomeMappability/kmers/k150/*kmer* data/TestGenomeMappability/kmers/k150/TEMPs
+mv data/TestGenomeMappability/kmers/k150/*bowtie* data/TestGenomeMappability/kmers/k150/TEMPs
